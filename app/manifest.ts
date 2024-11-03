becc782da723922119b48c0ce5de9be433e6858c
alexcanessa <canessa.alex@gmail.com>
2024-11-03 09:31:28 +0100

    feat: change application icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,6 +12,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#382d64",
     icons: [
       {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-192x192-maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
         src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
